244c2f068d7ebaceddbaec22827d74dfb9bb9285
ales2026 <ale.urankar@gmail.com>
2025-12-20 12:30:01 +0100

    moving default vertex shader to TextureEffect

--- MojFramework/TextureEffect.h
+++ MojFramework/TextureEffect.h
@@ -93,7 +93,35 @@ public:
         Vec2 t;
     };
 
-	typedef DefaultVertexShader<Vertex> VertexShader;
+    class VertexShader
+    {
+    public:
+        using Output = Vertex;
+        void BindWorld(const Mat4& world_in)
+        {
+            world = world_in;
+            worldProj = world * proj;
+        }
+        void BindProjection(const Mat4& proj_in)
+        {
+            proj = proj_in;
+            worldProj = world * proj;
+        }
+        const Mat4& GetProj() const
+        {
+            return proj;
+        }
+        Output operator()(const Vertex& v) const
+        {
+            Vec4 clipPos = v.pos * worldProj;
+
+            return Output(clipPos, v);
+        }
+    private:
+        Mat4 world = Mat4::Identity();
+        Mat4 proj = Mat4::Identity();
+        Mat4 worldProj = Mat4::Identity();
+    };
 
 	typedef DefaultGeometryShader<VertexShader::Output> GeometryShader;
 
@@ -109,7 +137,7 @@ public:
 			);
 			Vec3 tint = { 0.9f, 0.9f, 0.9f };
 			return texColor * tint;
-		}
+        }
 		void BindTexture(const Surface& tex)
 		{
 			pTex = &tex;
