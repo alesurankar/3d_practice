eaefe219a3919668e9d47875cb00ab5702cefeda
ales2026 <ale.urankar@gmail.com>
2025-12-20 10:30:25 +0100

    better TextureLightEffect, but not perfect

--- MojFramework/TextureLightEffect.h
+++ MojFramework/TextureLightEffect.h
@@ -8,20 +8,22 @@ public:
     class Vertex
     {
     public:
-        Vec4 pos;
-        Vec3 n;
-        Vec2 t;
 
         Vertex() = default;
-
         Vertex(const Vec3& pos3, const Vec3& normal, const Vec2& tex)
-            : pos(pos3, 1.0f), n(normal), t(tex) {
+            :
+            pos(pos3, 1.0f),
+            n(normal),
+            t(tex)
+        {
         }
-
         Vertex(const Vec4& pos4, const Vertex& src)
-            : pos(pos4), n(src.n), t(src.t) {
+            :
+            pos(pos4),
+            n(src.n),
+            t(src.t)
+        {
         }
-
         Vertex& operator+=(const Vertex& rhs)
         {
             pos += rhs.pos;
@@ -29,8 +31,47 @@ public:
             t += rhs.t;
             return *this;
         }
-        Vertex operator+(const Vertex& rhs) const { return Vertex(*this) += rhs; }
-        Vertex operator*(float s) const { return { pos * s, n * s, t * s }; }
+        Vertex operator+(const Vertex& rhs) const
+        {
+            return Vertex(*this) += rhs;
+        }
+        Vertex& operator-=(const Vertex& rhs)
+        {
+            pos -= rhs.pos;
+            n -= rhs.n;
+            t -= rhs.t;
+            return *this;
+        }
+        Vertex operator-(const Vertex& rhs) const
+        {
+            return Vertex(*this) -= rhs;
+        }
+        Vertex& operator*=(float rhs)
+        {
+            pos *= rhs;
+            n *= rhs;
+            t *= rhs;
+            return *this;
+        }
+        Vertex operator*(float rhs) const
+        {
+            return Vertex(*this) *= rhs;
+        }
+        Vertex& operator/=(float rhs)
+        {
+            pos /= rhs;
+            n /= rhs;
+            t /= rhs;
+            return *this;
+        }
+        Vertex operator/(float rhs) const
+        {
+            return Vertex(*this) /= rhs;
+        }
+    public:
+        Vec4 pos;
+        Vec3 n;
+        Vec2 t;
     };
 
     class VertexShader
@@ -39,12 +80,26 @@ public:
         class Output
         {
         public:
-            Vec4 pos;
-            Vec2 t;
-            Color color;
-
             Output() = default;
-
+            Output(const Vec4& pos_in)
+                :
+                pos(pos_in)
+            {
+            }
+            Output(const Vec4& pos_in, const Output& src)
+                :
+                pos(pos_in),
+                t(src.t),
+                color(src.color)
+            {
+            }
+            Output(const Vec4& pos_in, const Vec2& t_in, const Color& color_in)
+                :
+                pos(pos_in),
+                t(t_in),
+                color(color_in)
+            {
+            }
             Output& operator+=(const Output& rhs)
             {
                 pos += rhs.pos;
@@ -53,9 +108,10 @@ public:
                 color = Color(c + c2);
                 return *this;
             }
-
-            Output operator+(const Output& rhs) const { return Output(*this) += rhs; }
-
+            Output operator+(const Output& rhs) const 
+            { 
+                return Output(*this) += rhs;
+            }
             Output& operator-=(const Output& rhs)
             {
                 pos -= rhs.pos;
@@ -64,38 +120,45 @@ public:
                 color = Color(c - c2);
                 return *this;
             }
-
-            Output operator-(const Output& rhs) const { return Output(*this) -= rhs; }
-
-            Output& operator*=(float s)
+            Output operator-(const Output& rhs) const 
+            { 
+                return Output(*this) -= rhs;
+            }
+            Output& operator*=(float rhs)
             {
-                pos *= s;
-                t *= s;
+                pos *= rhs;
+                t *= rhs;
                 Vec3 c(color);
-                color = Color(c * s);
+                color = Color(c * rhs);
                 return *this;
             }
-
-            Output operator*(float s) const { return Output(*this) *= s; }
-
-            Output& operator/=(float s)
+            Output operator*(float rhs) const 
+            { 
+                return Output(*this) *= rhs;
+            }
+            Output& operator/=(float rhs)
             {
-                pos /= s;
-                t /= s;
+                pos /= rhs;
+                t /= rhs;
                 Vec3 c(color);
-                color = Color(c / s);
+                color = Color(c / rhs);
                 return *this;
             }
-
-            Output operator/(float s) const { return Output(*this) /= s; }
+            Output operator/(float rhs) const
+            { 
+                return Output(*this) /= rhs;
+            }
+        public:
+            Vec4 pos;
+            Vec2 t;
+            Color color;
         };
-
+    public:
         void BindWorld(const Mat4& world_in)
         {
             world = world_in;
-            worldProj = world * proj;
+            //worldProj = world * proj;
         }
-
         void BindProjection(const Mat4& proj_in)
         {
             proj = proj_in;
@@ -105,45 +168,45 @@ public:
         {
             return proj;
         }
-
         Output operator()(const Vertex& v) const
         {
             // Transform vertex position to clip space
             Vec4 clipPos = v.pos * worldProj;
 
-            // Transform normal using the upper-left 3x3 rotation part of the world matrix
-            Vec3 nWorld = {
-                v.n.x * world.elements[0][0] + v.n.y * world.elements[1][0] + v.n.z * world.elements[2][0],
-                v.n.x * world.elements[0][1] + v.n.y * world.elements[1][1] + v.n.z * world.elements[2][1],
-                v.n.x * world.elements[0][2] + v.n.y * world.elements[1][2] + v.n.z * world.elements[2][2]
-            };
-            nWorld = nWorld.GetNormalized();
+            Mat3 rotation;
+            for (int r = 0; r < 3; r++)
+                for (int c = 0; c < 3; c++)
+                    rotation.elements[r][c] = world.elements[r][c];
 
-            // Simple diffuse + ambient lighting
-            float intensity = std::max(0.0f, nWorld * -lightDir);
-            Vec3 lit = (ambient + diffuse * intensity).GetHadamard(materialColor).Saturate();
-
-            // Output
-            Output out;
-            out.pos = clipPos;
-            out.t = v.t;
-            out.color = Color(lit * 255.0f);
-            return out;
+            const auto d = diffuse * std::max(0.0f, -(v.n * rotation) * dir);
+            const auto c = color.GetHadamard(d + ambient).Saturate() * 255.0f;
+            return{ clipPos, v.t, Color(c) };
         }
-
-        void SetLightDirection(const Vec3& d) { lightDir = d.GetNormalized(); }
-        void SetMaterialColor(Color c) { materialColor = Vec3(c); }
-        void SetDiffuseLight(const Vec3& c) { diffuse = c; }
-        void SetAmbientLight(const Vec3& c) { ambient = c; }
-
+        void SetDiffuseLight(const Vec3& c)
+        {
+            diffuse = { c.x,c.y,c.z };
+        }
+        void SetAmbientLight(const Vec3& c)
+        {
+            ambient = { c.x,c.y,c.z };
+        }
+        void SetLightDirection(const Vec3& dl)
+        {
+            assert(dl.LenSq() >= 0.001f);
+            dir = dl.GetNormalized();
+        }
+        void SetMaterialColor(Color c)
+        {
+            color = Vec3(c);
+        }
     private:
         Mat4 world = Mat4::Identity();
         Mat4 proj = Mat4::Identity();
         Mat4 worldProj = Mat4::Identity();
-        Vec3 lightDir = { 0.0f, 0.0f, 1.0f };
+        Vec3 dir = { 0.0f, 0.0f, 1.0f };
         Vec3 diffuse = { 1.0f, 1.0f, 1.0f };
         Vec3 ambient = { 0.1f, 0.1f, 0.1f };
-        Vec3 materialColor = { 0.8f, 0.85f, 1.0f };
+        Vec3 color = { 0.8f, 0.85f, 1.0f };
     };
 
     using GeometryShader = DefaultGeometryShader<VertexShader::Output>;
@@ -154,18 +217,18 @@ public:
         template<class Input>
         Color operator()(const Input& in) const
         {
-            Color tex = pTex->GetPixel(
-                (unsigned int)std::min(in.t.x * tex_width + 0.5f, tex_xclamp),
-                (unsigned)std::min(in.t.y * texH + 0.5f, texH - 1)
+            Color texColor = pTex->GetPixel(
+                (unsigned int)std::min(in.t.x * texW + 0.5f, texW - 1.0f),
+                (unsigned int)std::min(in.t.y * texH + 0.5f, texH - 1.0f)
             );
-            return tex * Vec3(in.color);
+            return texColor *Vec3(in.color);
         }
 
         void BindTexture(const Surface& tex)
         {
             pTex = &tex;
-            texW = float(tex.GetWidth());
-            texH = float(tex.GetHeight());
+            texW = float(pTex->GetWidth());
+            texH = float(pTex->GetHeight());
         }
 
     private:
